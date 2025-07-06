4aa37369301504fb2538079152753a7036c1865a
Riste Dimitrievski <Riste@DESKTOP-BHPJ89K>
2025-07-06 14:44:44 +0200

    Created new API routes.

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+/**
+ * API Routes
+ */
+Route::middleware(\App\Http\Middleware\ApiMiddleware::class)->group(function(){
+    /**
+     * Route for handling login with JWT token
+     */
+   Route::controller(\App\Http\Controllers\LoginController::class)->group(function(){
+       Route::post("/login", 'login');
+   });
+});
