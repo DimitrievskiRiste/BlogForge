826f49a39ed053e854a4d4aca7075c1a5e03e889
Riste Dimitrievski <Riste@DESKTOP-BHPJ89K>
2025-07-07 19:56:02 +0200

    Created user logs table

---
+++ database/migrations/2025_07_07_172558_create_user_logs_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_logs', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->text('log_text');
+            $table->ipAddress('user_ip');
+            $table->string('user_device', 255);
+            $table->foreign('user_id')
+                ->on("users")
+                ->references("id")
+                ->onUpdate("cascade")
+                ->onDelete("cascade");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_logs');
+    }
+};
