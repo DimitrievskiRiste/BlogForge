73ba113ee3bcf30bae7d5f3a1cdf76b6a7433b7a
Riste Dimitrievski <Riste@DESKTOP-BHPJ89K>
2025-07-08 15:06:36 +0200

    Registered AuthorizedAPI Middleware in app.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,6 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->append(ApiMiddleware::class);
+        $middleware->append(\App\Http\Middleware\AuthorizeAPI::class);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
