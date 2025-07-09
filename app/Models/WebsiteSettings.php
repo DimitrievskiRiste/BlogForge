a4ca59d4a89605aabc0973844e643f5b89cd3e89
Riste Dimitrievski <Riste@DESKTOP-BHPJ89K>
2025-07-09 18:47:27 +0200

    Renamed fillable key logo_path to website_logo.

--- app/Models/WebsiteSettings.php
+++ app/Models/WebsiteSettings.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 /**
  * @property int $id
  * @property string $website_name
- * @property string $logo_path
+ * @property int $website_logo
  * @property boolean $registration_enabled
  * @property boolean $registration_min_age
  * @property boolean $verify_email_address
@@ -16,6 +16,6 @@ class WebsiteSettings extends Model
 {
     protected $table = 'website_settings';
     protected $primaryKey = 'id';
-    protected $fillable = ['website_name', 'logo_path', 'registration_enabled','registration_min_age','verify_email_address'];
+    protected $fillable = ['website_name', 'website_logo', 'registration_enabled','registration_min_age','verify_email_address'];
 
 }
