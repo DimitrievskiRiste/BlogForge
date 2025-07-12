92886d2d837afaa5fd72c9c05cab6130609c9039
Riste Dimitrievski <Riste@DESKTOP-BHPJ89K>
2025-07-12 17:51:55 +0200

    Implemented method getAuthenticatedAPIUser in Controller.php

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+use Illuminate\Http\Request;
+
 abstract class Controller
 {
-    //
+    protected function getAuthenticatedAPIUser(Request $request) :array
+    {
+        $header = explode(" ", $request->header("Authorization"));
+        list(1 => $token) = $header;
+        $secretPass = $request->header("Authorization-Pass");
+        $jwtToken = JWT::decode($token, new Key($secretPass, "HS256"));
+        $user = $this->findMember($jwtToken->user);
+        if(!is_null($user)){
+            return [
+                'user' => $user,
+                'token' => $jwtToken
+            ]
+        } else {
+            return [];
+        }
+    }
+    private function findMember(int $userId) :User|null{
+        return User::query()->with(['Group'])->find($userId) ?? null;
+    }
 }
