15b1e17d9033b358ef7326c66e8c9227c7cfd075
Riste Dimitrievski <Riste@DESKTOP-BHPJ89K>
2025-07-14 20:24:33 +0200

    Created scheduling artisan commands to handle synchronization between database data & cache storage.

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,16 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Schedule::command("cache:sync-users")->daily()->withoutOverlapping();
+Schedule::command("cache:sync-website-settings")->daily()->withoutOverlapping();
+Schedule::command("cache:sync-user-logs")->daily()->withoutOverlapping();
+Schedule::command("cache:sync-user-groups")->daily()->withoutOverlapping();
+Schedule::command("cache:sync-attachments")->daily()->withoutOverlapping();
+Schedule::command("cache:sync-categories")->daily()->withoutOverlapping();
+Schedule::command("cache:sync-user-attachments")->daily()->withoutOverlapping();
