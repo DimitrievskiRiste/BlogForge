671c8387da470b4337fb60dfc27365d4b5706fc7
Riste Dimitrievski <Riste@DESKTOP-BHPJ89K>
2025-07-15 17:20:57 +0200

    Created api middleware alias.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Middleware\AttachmentMiddleware;
+use App\Http\Middleware\AuthorizeAPI;
+use App\Http\Middleware\CategoryMiddleware;
+use App\Http\Middleware\EditCategoryMiddleware;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -12,12 +16,12 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->append(ApiMiddleware::class);
         $middleware->alias([
-            'auth.api' => \App\Http\Middleware\AuthorizeAPI::class, // Add alias
-            'scope.attachments' => \App\Http\Middleware\AttachmentMiddleware::class,
-            'scope.add_category' => \App\Http\Middleware\CategoryMiddleware::class,
-            'scope.edit_category' => \App\Http\Middleware\EditCategoryMiddleware::class,
+            'api' => ApiMiddleware::class,
+            'auth.api' => AuthorizeAPI::class, // Add alias
+            'scope.attachments' => AttachmentMiddleware::class,
+            'scope.add_category' => CategoryMiddleware::class,
+            'scope.edit_category' => EditCategoryMiddleware::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
