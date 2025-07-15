aa87712bb988dece67bb5c170218f13910268792
Riste Dimitrievski <Riste@DESKTOP-BHPJ89K>
2025-07-15 02:21:17 +0200

    Created array of models and for each of these models i've created schedule command for sync.

--- routes/console.php
+++ routes/console.php
@@ -7,7 +7,8 @@ use Illuminate\Support\Facades\Schedule;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
-
+/**
+To prevent DRY i've made code reusable.
 Schedule::command("cache:sync-users")->daily()->withoutOverlapping();
 Schedule::command("cache:sync-website-settings")->daily()->withoutOverlapping();
 Schedule::command("cache:sync-user-logs")->daily()->withoutOverlapping();
@@ -16,3 +17,17 @@ Schedule::command("cache:sync-attachments")->daily()->withoutOverlapping();
 Schedule::command("cache:sync-categories")->daily()->withoutOverlapping();
 Schedule::command("cache:sync-user-attachments")->daily()->withoutOverlapping();
 Schedule::command("cache:sync-articles")->daily()->withoutOverlapping();
+*/
+$models = [
+    "Articles",
+    "Attachments",
+    "Categories",
+    "User",
+    "UserAttachments",
+    "UserGroups",
+    "UserLogs",
+    "WebsiteSettings"
+];
+foreach($models as $model){
+    Schedule::command("cache:sync-models $model")->daily()->withoutOverlapping();
+}
