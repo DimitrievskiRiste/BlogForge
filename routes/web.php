a6a928affd19ba70f690aab2e4e5ae2d3d887faa
Riste Dimitrievski <Riste@DESKTOP-BHPJ89K>
2025-07-16 15:48:08 +0200

    Added api route for removal category with middleware auth.api and scope.remove_category.

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\AttachmentsController;
+use App\Http\Controllers\CategoriesController;
+use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -13,25 +16,27 @@ Route::middleware(['api'])->prefix("api")->withoutMiddleware(['web'])->group(fun
     /**
      * Route for handling login with JWT token
      */
-   Route::controller(\App\Http\Controllers\LoginController::class)->group(function(){
+   Route::controller(LoginController::class)->group(function(){
        Route::post("/login", 'login');
    });
    // Authorized API Routes. Required JWT Token for access
     Route::middleware(['auth.api','scope.attachments'])->group(function(){
-        Route::controller(\App\Http\Controllers\AttachmentsController::class)->group(function(){
+        Route::controller(AttachmentsController::class)->group(function(){
             Route::post("/attachments/upload", 'upload');
         });
     });
 
     // Route for Categories. Currently implemented list categories and adding new category
-    Route::controller(\App\Http\Controllers\CategoriesController::class)->group(function(){
-        Route::get("/categories", "actionList");
+    Route::controller(CategoriesController::class)->group(function(){
+        Route::get("/categories", "list");
         // scope add_category
-        Route::post("/category/add", "actionAdd")->middleware(['auth.api','scope.add_category']);
+        Route::post("/category/add", "add")->middleware(['auth.api','scope.add_category']);
 
-        Route::get('/category_info/{slug}','actionGet');
+        Route::get('/category_info/{slug}','get');
         // Scope edit_category
-        Route::post("/edit_category", 'actionEdit')->middleware(['auth.api','scope.edit_category']);
+        Route::post("/edit_category", 'edit')->middleware(['auth.api','scope.edit_category']);
+        // Scope remove_category
+        Route::post('/remove_category', 'delete')->middleware(['auth.api', 'scope.remove_category']);
     });
 
 });
