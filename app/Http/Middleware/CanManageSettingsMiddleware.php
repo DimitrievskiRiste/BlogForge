1cb13c5c7b426c11546694e769acc849f2240761
Riste Dimitrievski <Riste@DESKTOP-BHPJ89K>
2025-07-18 01:03:14 +0200

    Created api middleware for scope "scope.can_change_settings" for website settings protected routes.

---
+++ app/Http/Middleware/CanManageSettingsMiddleware.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Models\User;
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CanManageSettingsMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $userId = $request->attributes->get('userId');
+        $isValidated = $request->attributes->get('isValidated');
+        if(is_null($userId) || is_null($isValidated)) {
+            return response()->json(['hasErrors' => true, 'message' => 'Missing permission can_change_settings'], 403);
+        }
+        $member = $this->findMember($userId);
+        if(!is_null($member) && $member->Group->can_access_admincp && $member->Group->can_change_settings) {
+            return $next($request);
+        }
+        return response()->json(['hasErrors' => true, 'message' => 'Missing permission can_change_settings'], 403);
+    }
+    private function findMember(int $userId) :User|null {
+        return User::query()->with('Group')->find($userId);
+    }
+}
