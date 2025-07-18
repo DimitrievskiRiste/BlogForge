2b1402c85d244e8ee62c2f0baf031ada05d02afb
Riste Dimitrievski <Riste@DESKTOP-BHPJ89K>
2025-07-18 22:22:40 +0200

    Created migration table for content translations

---
+++ database/migrations/2025_07_18_174953_create_content_translations_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('content_translations', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('article_id');
+            $table->longText('content');
+            $table->string('locale');
+            $table->foreign('article_id')
+                ->on("articles")
+                ->references('id')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('content_translations');
+    }
+};
