fbfbca723f9460fdb7c9b6c9fe0480860a552880
Riste Dimitrievski <Riste@DESKTOP-BHPJ89K>
2025-07-24 13:44:56 +0200

    Added new middleware for scope.can_remove_articles

---
+++ app/Http/Middleware/CanRemoveArticlesMiddleware.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Models\User;
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CanRemoveArticlesMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $userId = $request->attributes->get('userId') ?? null;
+        $isValidated = $request->attributes->get('IsValidated') ?? null;
+        if(is_null($userId) || is_null($isValidated)) {
+            return response()->json(['hasErrors' => true, 'message' => 'Access is denied'], 403);
+        }
+        $user = $this->findUser($userId);
+        if(!is_null($user) && $user->Group->can_remove_article && $user->Group->can_access_admincp) {
+            return $next($request);
+        }
+        return response()->json(['hasErrors' => true, 'message' => 'Access is denied'], 403);
+    }
+    protected function findUser(int $userId) :User|null
+    {
+        return User::with(['Group'])->find($userId);
+    }
+}
