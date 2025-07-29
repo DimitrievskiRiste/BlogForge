da5dadf395de529722ebde1cb61406bb762dcf1a
Riste Dimitrievski <Riste@DESKTOP-BHPJ89K>
2025-07-30 00:57:51 +0200

    Registered api route for editing comment.

--- routes/web.php
+++ routes/web.php
@@ -64,5 +64,6 @@ Route::middleware(['api'])->prefix("api")->withoutMiddleware(['web'])->group(fun
     // Routes for comments
     Route::controller(CommentsController::class)->group(function(){
         Route::post('/comment','add')->middleware(['auth.api','scope.can_comment_on_article']);
+        Route::post('/edit_comment', 'edit')->middleware(['auth.api', 'scope.can_edit_comment']);
     });
 });
