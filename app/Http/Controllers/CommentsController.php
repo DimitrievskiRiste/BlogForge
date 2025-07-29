33a6fadf08f9302ad5cedb3e5aab010896a74a40
Riste Dimitrievski <Riste@DESKTOP-BHPJ89K>
2025-07-30 00:57:32 +0200

    Implemented API feature for edit a comment.

--- app/Http/Controllers/CommentsController.php
+++ app/Http/Controllers/CommentsController.php
@@ -28,4 +28,34 @@ class CommentsController extends Controller
             return response()->json(['hasErrors' => true, 'message' => 'Cant add new comment. Please check error logs'], 500);
         }
     }
+    public function edit(Request $request) :Response {
+        try {
+            $data = $request->validate([
+                'comment' => 'required',
+                'id' => 'required|integer'
+            ]);
+            $comment = Comments::query()->find($data['id']);
+            $isAdmin = $request->attributes->get('isAdmin') ?? false;
+            $userId =  $request->attributes->get('userId');
+            if($isAdmin) {
+                // If user is admin, he can edit any comment he want.
+                $comment->comment = $data['comment'];
+                $comment->save();
+                $this->loadRepo("Comments")->addOrUpdate($comment);
+                return response()->json(['success' => true, 'message' => 'Comment edited successfully']);
+            }
+                // User is regular user, not an admin. We will check for comment id and user id.
+                if($comment->user_id !== $userId) {
+                    return response()->json(['hasErrors' => true, 'message' => 'Not authorized to perform this action'], 403);
+                }
+                $comment->comment = $data['comment'];
+                $comment->save();
+                $this->loadRepo('Comments')->addOrUpdate($comment);
+                return response()->json(['success' => true, 'message' => 'Comment successfully edited']);
+        }
+        catch(\Exception $e) {
+            Log::error($e);
+            return response()->json(['hasErrors' => true, 'message' => 'Cant edit comment, please check error logs for more details'], 500);
+        }
+    }
 }
