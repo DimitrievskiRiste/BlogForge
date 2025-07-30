2cc44edb6853e5d27da749efd1b0cb00988f7277
Riste Dimitrievski <Riste@DESKTOP-BHPJ89K>
2025-07-30 23:53:38 +0200

    Created Database class for web installation script.

---
+++ app/Installer/Database.php
@@ -0,0 +1,58 @@
+<?php
+namespace App\Installer;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Schema;
+
+class Database
+{
+    protected static array $defaultTables = [
+        "users",
+        "cache",
+        "jobs",
+        "user_groups",
+        "attachments",
+        "user_logs",
+        "website_settings",
+        "user_attachments",
+        "categories",
+        "articles",
+        "content_translations",
+        "comments"
+    ];
+    /**
+     * Get database tables
+     * @return array
+     */
+    public function getDatabaseTables() :array
+    {
+        return Schema::getTables();
+    }
+
+    /**
+     * Return bool true if db is empty, false otherwise
+     * @return bool
+     */
+    public function isDbEmpty():bool {
+        return (empty(self::getDatabaseTables()) ? true : false);
+    }
+
+    /**
+     * Check DB connection
+     * @return bool
+     */
+    public function checkConnection():bool{
+        try {
+            DB::connection()->getPdo();
+            return true;
+        } catch(\Exception $e) {
+            Log::error($e);
+            return false;
+        }
+    }
+    public function createDBTables() :void
+    {
+        Artisan::call("blogforge:migrate");
+    }
+}
