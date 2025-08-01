d5f9b64f6cbdc6bfb6f281ef257904e18617f1d8
Riste Dimitrievski <Riste@DESKTOP-BHPJ89K>
2025-08-01 18:38:16 +0200

    Created installer middleware with alis api.installer

---
+++ app/Http/Middleware/InstallerMiddleware.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Installer\Installer;
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class InstallerMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        return !Installer::isLocked() ? $next($request) : response()->json(['hasErrors' => true, 'message' => 'Installation is locked!'],400);
+    }
+}
